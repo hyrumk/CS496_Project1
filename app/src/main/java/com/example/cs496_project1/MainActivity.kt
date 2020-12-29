acba15b5e2450b9837327e7c78acb3e0378c6b8d
hyrumk <60143710+hyrumk@users.noreply.github.com>
2020-12-29 17:44:51 +0900

    Add function
    read(): import and display contact details through ListView

--- app/src/main/java/com/example/cs496_project1/MainActivity.kt
+++ app/src/main/java/com/example/cs496_project1/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.cs496_project1
 
+import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.ListView
+import android.widget.SimpleCursorAdapter
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        read()
+    }
+
+    fun read(){
+        val listview = findViewById<ListView>(R.id.listview)
+        var cursor: Cursor? = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null)
+        startManagingCursor(cursor)
+
+        var from = arrayOf(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone._ID)
+
+        var to = intArrayOf(android.R.id.text1, android.R.id.text2)
+        var simple: SimpleCursorAdapter = SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, cursor, from, to)
+
+        listview.adapter = simple
     }
 }
